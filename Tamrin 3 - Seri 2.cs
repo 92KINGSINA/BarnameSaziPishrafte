22f76ff28815ff8cd28b3371b6225767c830c885
92KINGSINA <94956841+92KINGSINA@users.noreply.github.com>
2024-11-04 23:16:24 +0330

    Add files via upload

---
+++ Tamrin 3 - Seri 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (IsFibonacci(n))
+        {
+            Console.WriteLine($"{n} Is in Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} Isn't in Fibonacci.");
+        }
+    }
+
+    static bool IsFibonacci(int n)
+    {
+        int a = 0, b = 1, temp;
+
+        if (n < 0)
+            return false;
+
+        while (b < n)
+        {
+            temp = b;
+            b = a + b;
+            a = temp;
+        }
+
+        return b == n;
+    }
+}
