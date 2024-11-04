cf35b07d111c45246824fa1a1c062bc1c301874c
92KINGSINA <94956841+92KINGSINA@users.noreply.github.com>
2024-11-04 23:47:12 +0330

    Add files via upload

---
+++ Tamrin 5 - Seri 2.cs
@@ -0,0 +1,18 @@
+﻿namespace Tamrin_5___Seri_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just Press Letters");
+            char b;
+            do
+            {
+                b = Console.ReadKey(true).KeyChar;
+            }
+            while (b != 'q' && b != 'w' && b != 'e' && b != 'r' && b != 't' && b != 'y' && b != 'u' && b != 'i' && b != 'o' && b != 'p' && b != 'a' && b != 's' && b != 'd' && b != 'f' && b != 'g' && b != 'h' && b != 'j' && b != 'k' && b != 'l' && b != 'z' && b != 'x' && b != 'c' && b != 'v' && b != 'b' && b != 'n' && b != 'm');
+            Console.WriteLine();
+            Console.WriteLine("You Pressed " + b);
+        }
+    }
+}
