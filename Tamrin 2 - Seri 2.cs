fab9c7125bd0c6b90c34fd5efbed445ebd8e96f1
92KINGSINA <94956841+92KINGSINA@users.noreply.github.com>
2024-11-04 23:09:36 +0330

    Add files via upload

---
+++ Tamrin 2 - Seri 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number (n): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        if (n > m)
+        {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        Console.WriteLine($"Palindromic numbers between {n} and {m}:");
+
+        for (int i = n; i <= m; i++)
+        {
+            if (IsPalindrome(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        string strNumber = number.ToString();
+        char[] charArray = strNumber.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedNumber = new string(charArray);
+        return strNumber == reversedNumber;
+    }
+}
