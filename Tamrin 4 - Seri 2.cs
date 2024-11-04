e9e1dc3445505ecb17e578a15e1a1ae50f1db1d6
92KINGSINA <94956841+92KINGSINA@users.noreply.github.com>
2024-11-04 23:34:58 +0330

    Add files via upload

---
+++ Tamrin 4 - Seri 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Just Press Numbers");
+        char b;
+        do
+        {
+            b = Console.ReadKey(true).KeyChar;
+        }
+        while (b != '0' && b != '1' && b != '2' && b != '3' && b != '4' && b != '5' && b != '6' && b != '7' && b != '8' && b != '9');
+        Console.WriteLine();
+        Console.WriteLine("You Pressed " + b);
+
+    }
+}
